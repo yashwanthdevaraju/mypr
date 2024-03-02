f4f6c3490b1392fdfd768351f1b3bf50f1f63dac
yashwanthdevaraju <161132895+yashwanthdevaraju@users.noreply.github.com>
2024-03-02 07:44:19 +0530

    Add files via upload

---
+++ courier-management-system-main/home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #2ab7ca;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">Copyright © 2024 Designed By Sagar and Yashwanth</p><br>
+  <a href="mailto:tuntun@gmail.com" style="color:brown">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
